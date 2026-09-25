18eefd01798198985418dceba257a87136992ad6
Yutaka Imagawa <flugel.biz@gmail.com>
2026-09-25 15:14:36 +0900

    Meta 機構の反映とエラーページの改修

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import solidJs from '@astrojs/solid-js';
 import svelte from '@astrojs/svelte';
 import vue from '@astrojs/vue';
+import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +13,10 @@ export default defineConfig({
         }),
 		svelte(),
 		vue()
-	]
+	],
+  vite: {
+	plugins: [
+		yaml(),
+	],
+  },
 });
